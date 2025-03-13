879dd9ab1260f3a17d8773a6a876532607a2f29e
josemnmatos <josemnmatos@gmail.com>
2025-03-13 00:08:22 +0000

    first commit

---
+++ custom_models/mlp.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+
+
+class MLP(nn.Module):
+    def __init__(self, input_size, hidden_sizes, num_classes):
+        super(MLP, self).__init__()
+        self.layers = nn.ModuleList()
+        self.activations = nn.ModuleList()
+
+        if len(hidden_sizes) == 0:
+            self.layers.append(nn.Linear(input_size, num_classes))
+        else:
+            self.layers.append(nn.Linear(input_size, hidden_sizes[0]))
+            self.activations.append(nn.ReLU())
+
+            for i in range(len(hidden_sizes) - 1):
+                self.layers.append(nn.Linear(hidden_sizes[i - 1], hidden_sizes[i]))
+                self.activations.append(nn.ReLU())
+
+            self.layers.append(nn.Linear(hidden_sizes[-1], num_classes))
+
+    def forward(self, x):
+        for layer, activation in zip(self.layers, self.activations):
+            x = activation(layer(x))
+        return x
